214cb465a3bf4aba1b1882581adc51494b998d91
s3585513 <HundredEyesMonk@users.noreply.github.com>
2020-04-27 21:34:45 +1000

    Update Deletecars.py

--- Deletecars.py
+++ Deletecars.py
@@ -1,6 +1,13 @@
-class Deletecars
-  def delete_task(db, id)：
-   sql = 'DELETE FROM tasks WHERE id=?'
-    cursor = db.cursor()
-    cursor.execute(sql, (id,))
-    conn.commit()
+class Deletecars:
+   def delete_task(db, id):
+      with utilsObj.connection.cursor() as db:
+      """
+      Delete a task by task id
+      :param conn:  Connection to the SQLite databas
+      :param id: id of the task
+      :return:
+      """
+      sql = 'DELETE FROM tasks WHERE id=?'
+      cursor = db.cursor()
+      cursor.execute(sql, (id,))
+      conn.commit()
